10f5b25d2256613f87cee172b4118a5fd7119f03
vainiovesa <vesa.vainio@helsinki.fi>
2025-03-17 19:35:57 +0200

    Add login and logout

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import sqlite3
 from flask import Flask
-from flask import render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import config
 import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return "Lahjalistasovellus"
+    return render_template("index.html")
 
 @app.route("/register")
 def register():
@@ -28,3 +30,23 @@ def create():
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
     return "Tunnus luotu"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        password_hash = db.query(sql, [username])[0][0]
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")

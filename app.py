f0c7155644a7b8ce922f8eea2b0ede7c1a40d536
vainiovesa <vesa.vainio@helsinki.fi>
2025-03-17 18:58:03 +0200

    Add registering

---
+++ app.py
@@ -0,0 +1,30 @@
+import sqlite3
+from flask import Flask
+from flask import render_template, request
+from werkzeug.security import generate_password_hash
+import db
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Lahjalistasovellus"
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+    return "Tunnus luotu"
